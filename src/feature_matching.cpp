fedde9071d95fc7a3de416423ed5961ff57945b1
m.takeuchi <t190d074@gunma-u.ac.jp>
2023-05-31 15:57:40 +0900

    [fix]ORBの引数変更

--- src/feature_matching.cpp
+++ src/feature_matching.cpp
@@ -20,13 +20,13 @@ void feature_matching(const cv::Mat &src1, const cv::Mat &src2, cv::Mat &dst)
   /* 比較のために複数手法を記述 必要に応じてコメントアウト*/
   /* 特徴点検出*/
   /* AKAZE */
-  // cv::Ptr<cv::AKAZE> akaze = cv::AKAZE::create(cv::AKAZE::DESCRIPTOR_MLDB, 0, 3, 0.001f);
+  // cv::Ptr<cv::AKAZE> akaze = cv::AKAZE::create();
   // akaze->detect(src1, key1);
   // akaze->detect(src2, key2);
   // akaze->compute(src1, key1, des1); 
   // akaze->compute(src2, key2, des2);
   /* ORB */
-  cv::Ptr<cv::ORB> orb = cv::ORB::create(100);
+  cv::Ptr<cv::ORB> orb = cv::ORB::create();
   orb->detect(src1, key1);
   orb->detect(src2, key2);
   orb->compute(src1, key1, des1); 
