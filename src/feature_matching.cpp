e6f6a019ac19d18a8e1e3c87955d1ad02caa092e
m.takeuchi <t190d074@gunma-u.ac.jp>
2023-05-31 15:37:58 +0900

    [fix]射影変換による画像の補正を実装

--- src/feature_matching.cpp
+++ src/feature_matching.cpp
@@ -2,6 +2,14 @@
 #include <iostream>
 #include <numeric>
 
+/*  特徴点マッチングによる類似度計算及び入力画像の補正(射影変換)を行う関数 
+    src1 : 入力画像(変形される)
+    src2 : 入力画像
+    dst  : 出力画像(src1をsrc2に合わせて変形した画像)
+    
+    動作確認済
+    ・特徴点検出:ORB + 特徴点マッチング:総当たり */
+
 void feature_matching(const cv::Mat &src1, const cv::Mat &src2, cv::Mat &dst)
 {
   std::vector<cv::KeyPoint> key1, key2; // 特徴点を格納
@@ -41,14 +49,15 @@ void feature_matching(const cv::Mat &src1, const cv::Mat &src2, cv::Mat &dst)
       match.push_back(forward);
     }
   }
-  cv::drawMatches(src1, key1, src2, key2, match, dst);
+  // cv::drawMatches(src1, key1, src2, key2, match, dst);
 
+  /* 類似度計算(距離による実装、0に近い値ほど画像が類似) */
   for(int i = 0; i < match.size(); i++){
     cv::DMatch dis = match[i];
     sim += dis.distance;
   }
   sim /= match.size();
-  std::cout << "類似度: " << sim << std::endl; 
+  //std::cout << "類似度: " << sim << std::endl; 
 
   /* 画像の類似度が低すぎる場合は終了 */
   if(0/* sim > THRESHOLD*/){
@@ -56,4 +65,18 @@ void feature_matching(const cv::Mat &src1, const cv::Mat &src2, cv::Mat &dst)
     std::exit(1);
   }
 
+  /* src1をsrc2に合わせる形で射影変換して補正 */
+  std::vector<cv::Vec2f> get_pt1(match.size()), get_pt2(match.size()); // 使用する特徴点
+  /* 対応する特徴点の座標を取得・格納*/
+  for(int i = 0; i < match.size(); i++){
+    get_pt1[i][0] = key1[match[i].queryIdx].pt.x;
+    get_pt1[i][1] = key1[match[i].queryIdx].pt.y;
+    get_pt2[i][0] = key2[match[i].trainIdx].pt.x;
+    get_pt2[i][1] = key2[match[i].trainIdx].pt.y;
+  }
+
+  /* ホモグラフィ行列推定 */
+  cv::Mat H = cv::findHomography(get_pt1, get_pt2, cv::RANSAC); 
+  /* src1を変形 */
+  cv::warpPerspective(src1, dst, H, src2.size());
 }
