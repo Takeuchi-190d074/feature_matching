ed6142f638a699bec4aee3b396b1bda8b959ca30
m.takeuchi <t190d074@gunma-u.ac.jp>
2023-06-07 14:51:54 +0900

    [fix]精度の高い特徴点50箇所のみを用いて補正を行う形に

--- src/feature_matching.cpp
+++ src/feature_matching.cpp
@@ -1,6 +1,5 @@
 #include <opencv2/opencv.hpp>
 #include <iostream>
-#include <numeric>
 
 /*  特徴点マッチングによる類似度計算及び入力画像の補正(射影変換)を行う関数 
     src1 : 入力画像(変形される)
@@ -8,7 +7,11 @@
     dst  : 出力画像(src1をsrc2に合わせて変形した画像)
     
     動作確認済
-    ・特徴点検出:ORB + 特徴点マッチング:総当たり */
+    1. 特徴点検出:ORB   + 特徴点マッチング:総当たり
+    2. 特徴点検出:AKAZE + 特徴点マッチング:総当たり
+    
+    必要処理時間: 1 < 2 
+    精度        : 1 < 2    */
 
 void feature_matching(const cv::Mat &src1, const cv::Mat &src2, cv::Mat &dst)
 {
@@ -31,25 +34,30 @@ void feature_matching(const cv::Mat &src1, const cv::Mat &src2, cv::Mat &dst)
   orb->detect(src2, key2);
   orb->compute(src1, key1, des1); 
   orb->compute(src2, key2, des2);
-  // std::cout << des1 << std::endl;
+
+  //std::cout << des1 << std::endl;
 
   /* 特徴点マッチングアルゴリズム */
   /* 総当たり */
   cv::Ptr<cv::DescriptorMatcher> matcher = cv::DescriptorMatcher::create("BruteForce");
 
   /* 特徴点マッチング */
-  /* クロスチェックを行い、両方でマッチしたものだけ残す */
   std::vector<cv::DMatch> match, match12, match21;
-  matcher->match(des1, des2, match12);
-  matcher->match(des2, des1, match21);
-  for(int i = 0; i < match12.size(); i++){
-    cv::DMatch forward = match12[i];
-    cv::DMatch backward = match21[forward.trainIdx];
-    if (backward.trainIdx == forward.queryIdx){
-      match.push_back(forward);
+  matcher->match(des1, des2, match);
+
+  /* 特徴量距離の小さい順にソートし、不要な点を削除 */
+  for(int i = 0; i < match.size(); i++){
+    double min = match[i].distance;
+    int n = i;
+    for(int j = i + 1; j < match.size(); j++){
+      if(min > match[j].distance){
+        n = j;
+        min = match[j].distance;
+      }
     }
+    std::swap(match[i], match[n]);
   }
-  // cv::drawMatches(src1, key1, src2, key2, match, dst);
+  match.erase(match.begin() + 50, match.end());
 
   /* 類似度計算(距離による実装、0に近い値ほど画像が類似) */
   for(int i = 0; i < match.size(); i++){
@@ -57,13 +65,15 @@ void feature_matching(const cv::Mat &src1, const cv::Mat &src2, cv::Mat &dst)
     sim += dis.distance;
   }
   sim /= match.size();
-  //std::cout << "類似度: " << sim << std::endl; 
+  std::cout << "類似度: " << sim << std::endl; 
 
   /* 画像の類似度が低すぎる場合は終了 */
   if(0/* sim > THRESHOLD*/){
     std::cerr << "画像が違いすぎます" << std::endl;
     std::exit(1);
   }
+
+  // cv::drawMatches(src1, key1, src2, key2, match, dst);
 
   /* src1をsrc2に合わせる形で射影変換して補正 */
   std::vector<cv::Vec2f> get_pt1(match.size()), get_pt2(match.size()); // 使用する特徴点
